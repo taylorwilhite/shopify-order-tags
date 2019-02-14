2c754c42d251af5781b18d660fdc5de3f2419a51
Taylor Wilhite <taylorwilhite@gmail.com>
2019-02-14 13:35:56 -0800

    add route for webhook entry

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const express = require('express');
 
 const app = express();
 
+app.get('/neworder', (req, res) => {
+  console.log('webhook fired!');
+});
+
 app.listen(3000, () => {
   console.log('Server is running on port 3000!');
 });
