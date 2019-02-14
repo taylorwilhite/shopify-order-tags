5a6a551068f14df80d8a3f6876cd0a7c001ec4db
Taylor Wilhite <taylorwilhite@gmail.com>
2019-02-14 13:42:13 -0800

    add dotenv

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require('express');
 
+require('dotenv').load();
+
 const app = express();
 
 app.get('/neworder', (req, res) => {
   console.log('webhook fired!');
 });
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log('Server is running on port 3000!');
 });
