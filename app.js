4d446196e2a5ad0475f3c1a249206fe8dcd73c3e
Taylor Wilhite <taylorwilhite@gmail.com>
2019-02-14 14:36:20 -0800

    add bodyparser to read webhook body

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 require('dotenv').load();
 
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
 app.post('/neworder', (req, res) => {
   console.log('webhook fired!');
+  res.send(200);
+  console.log(req.body);
 });
 
 app.listen(process.env.PORT || 3000, () => {
