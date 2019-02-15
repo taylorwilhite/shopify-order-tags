bba616642d0e0b2b99d1a2c6100915085b4ee70f
Taylor Wilhite <taylorwilhite@gmail.com>
2019-02-15 09:43:09 -0800

    fix exports

--- functions/index.js
+++ functions/index.js
@@ -1,19 +1,17 @@
 const request = require('request');
 
-function getProductTags(lineItems) {
+exports.getProductTags = (lineItems) => {
   const ids = lineItems.map(lineItem => lineItem.id);
-  const url = `https://${process.env.SHOPIFY_API_KEY}:${process.env.SHOPIFY_API_PASSWORD}@amaryllis-land.myshopify.com/admin/products.json?ids=${ids.split()}`
+  const url = `https://${process.env.SHOPIFY_API_KEY}:${process.env.SHOPIFY_API_PASSWORD}@amaryllis-land.myshopify.com/admin/products.json?ids=${ids.split()}`;
   const tags = request.get(url)
     .products.map(product => product.tags).join(', ').split(', ');
   return tags;
-}
+};
 
-function addShipstationTag(orderId) {
+exports.addShipstationTag = (orderId) => {
 
-}
+};
 
-function hasSale(tagArray) {
+exports.hasSale = (tagArray) => {
 
-}
-
-export { getProductTags, addShipstationTag, hasSale };
+};
