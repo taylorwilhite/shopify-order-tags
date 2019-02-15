ccfbd915fe8e44f9d726270c76cba9c67450f52f
Taylor Wilhite <taylorwilhite@gmail.com>
2019-02-15 09:45:21 -0800

    add logging

--- functions/index.js
+++ functions/index.js
@@ -2,6 +2,7 @@ const request = require('request');
 
 exports.getProductTags = (lineItems) => {
   const ids = lineItems.map(lineItem => lineItem.id);
+  console.log(ids);
   const url = `https://${process.env.SHOPIFY_API_KEY}:${process.env.SHOPIFY_API_PASSWORD}@amaryllis-land.myshopify.com/admin/products.json?ids=${ids.split()}`;
   const tags = request.get(url)
     .products.map(product => product.tags).join(', ').split(', ');
