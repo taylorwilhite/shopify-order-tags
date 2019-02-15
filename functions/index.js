1c6ec0f5315f1a3b853d94f2cdc070d60501953e
Taylor Wilhite <taylorwilhite@gmail.com>
2019-02-15 09:51:31 -0800

    test request

--- functions/index.js
+++ functions/index.js
@@ -4,7 +4,8 @@ exports.getProductTags = (lineItems) => {
   const ids = lineItems.map(lineItem => lineItem.id);
   const url = `https://${process.env.SHOPIFY_API_KEY}:${process.env.SHOPIFY_API_PASSWORD}@amaryllis-land.myshopify.com/admin/products.json?ids=${ids.join()}`;
   const tags = request.get(url)
-    .products.map(product => product.tags).join(', ').split(', ');
+    // .products.map(product => product.tags).join(', ').split(', ');
+  console.log(tags);
   return tags;
 };
 
